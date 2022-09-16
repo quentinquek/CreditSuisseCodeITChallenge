f7cc82a7b35c5c280de1cd0b04ec2d17ef5f1827
quentinquek <quentinquek@hotmail.com>
2022-09-16 11:19:24 +0800

    make it better

--- App.py
+++ App.py
@@ -8,6 +8,11 @@ def default_route():
     return "Python Template"
 
 
+@app.route("/health")
+def health():
+    return "url is working"
+
+
 logger = logging.getLogger()
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
