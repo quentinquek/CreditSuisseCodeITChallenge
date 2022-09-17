f465d2b6704f4d1edfdaeec202d63a616a17fc74
quentinquek <quentinquek@hotmail.com>
2022-09-17 13:03:10 +0800

    bug fixes

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-# import codeitsuisse.routes.square
+import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
 import codeitsuisse.routes.tickerStreamPart2
 import codeitsuisse.routes.cryptocollapz
