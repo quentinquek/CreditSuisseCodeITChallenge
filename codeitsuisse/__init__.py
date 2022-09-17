a9daede427ae9cd8cf0356434d41af1f5f96506c
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2022-09-17 10:58:07 +0800

    Added cryptocollapz code

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 # import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
 import codeitsuisse.routes.tickerStreamPart2
+import codeitsuisse.routes.cryptocollapz
+
 
 
 
